f1b66feb82818e77c621b4f95caa3bfce3bc0c50
mohanreddy3 <mohansaireddykammakatla@gmail.com>
2024-09-18 22:06:42 +0530

    18-09-24

---
+++ Cognizant_Interview/p5.java
@@ -0,0 +1,16 @@
+//Amstrong Number
+// It's a number that equals the sum of its digits, each raised to a power. 
+// For example, if you have a number like 153, it's an Armstrong number because 1^3 + 5^3 + 3^3 equals 153.
+package Cognizant_Interview;
+
+// import java.util.Scanner;
+
+public class p5 {
+    //main
+    // public static void main(String[] args) {
+    //     Scanner ip = new Scanner(System.in);
+    //     int num = ip.nextInt();
+        
+    // }
+    
+}
