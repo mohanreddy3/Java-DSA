801c14b23b075c67c7ab02d9fdce574b9f446d4d
mohanreddy3 <mohansaireddykammakatla@gmail.com>
2024-09-22 15:22:47 +0530

    22-09-24

---
+++ Strings/k2.java
@@ -0,0 +1,16 @@
+// Character Array & Diff Concepts 
+package Strings;
+
+import java.util.Arrays;
+
+public class k2 {
+    public static void main(String[] args) {
+        String name = "Mohan Sai Reddy";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toUpperCase());
+        System.out.println(name.indexOf("n"));
+        System.out.println("    Mohan    ".strip());
+        System.out.println(Arrays.toString("zzabbcaeeff".split("a")));
+    }
+    
+}
