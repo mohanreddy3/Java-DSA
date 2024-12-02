7e72584d00f9ccc0401287c6c1308449254342dc
mohanreddy3 <mohansaireddykammakatla@gmail.com>
2024-12-02 13:21:24 +0530

    2-12-24

--- CapgeminiQues/Cp7.java
+++ CapgeminiQues/Cp7.java
@@ -13,9 +13,6 @@ Output:
  Explanation: The triplets satisfy the condition 
  a² + b² = c² , where a , b, and c are integers
  */
-
-
-
 package CapgeminiQues;
 
 import java.util.Scanner;
@@ -23,11 +20,19 @@ import java.util.Scanner;
 public class Cp7 {
     public static void main(String[] args) {
         Scanner ip = new Scanner(System.in);
+        System.out.println("Enter Limit: ");
         int limit = ip.nextInt();
-        for(int i=0; i<limit; i++){
-            int triplet;
-
+        for(int a=1; a<=limit; a++){
+            for(int b=a+1; b<=limit; b++){
+                //c2 = a2 + b2
+                int cSquare = (a*a) +(b*b);
+                int c = (int)Math.sqrt(cSquare);
+                // checking
+                if( c*c == cSquare && c<=limit){
+                    System.out.println(a + " " + b + " " + c);
+                }
+            }
+        }   
+        ip.close(); 
         }
     }
-    
-}
