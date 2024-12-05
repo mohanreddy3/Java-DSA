9cb8f818036ae9372e77ff50ad7ad8765ff51e76
mohanreddy3 <mohansaireddykammakatla@gmail.com>
2024-12-06 00:10:39 +0530

    6-12-24

--- Trees/ApnaClg/BT.java
+++ Trees/ApnaClg/BT.java
@@ -16,10 +16,21 @@ public class BT {
         static int indx = -1;
         public static Node buildTree(int[] nodes){
             indx++;
-            if(nodes[indx] == null){
-                return;
+            if(nodes[indx] == -1){
+                return null;
             }
+            Node newnode = new Node(nodes[indx]);
+                newnode.left = buildTree(nodes);
+                newnode.right = buildTree(nodes);
+            return newnode;
         }
+    }
+    //main
+    public static void main(String[] args){
+        int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        // Binarytree tree = new Binarytree();
+        Node root =  Binarytree.buildTree(nodes);
+        System.out.println(root.data);
     }
     
 }
