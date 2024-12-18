126dedd6787a6c7e09aca1fe2174241d3d1bfce2
mohanreddy3 <mohansaireddykammakatla@gmail.com>
2024-12-18 23:38:41 +0530

    18-12-24

--- Trees/ApnaClg/Practice/MinDistBtw2Nodes.java
+++ Trees/ApnaClg/Practice/MinDistBtw2Nodes.java
@@ -35,23 +35,37 @@ public class MinDistBtw2Nodes {
         // last case if (rightLca && leftLca != null)
         return root;
     }
+    //main logic
+    //To Find Min dist btw 2 nodes
+    public static int minDist(Node root, int n1, int n2){
+        Node lca = findLca(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+         return dist1+dist2;
+    }
+    //helper fun() -> to find distance between lca and node
+    public static int lcaDist(Node root, int n){
+        //base case
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+        //check left and right childs
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if( leftDist == -1 && rightDist == -1){
+            return -1;
+        }
+        else if(leftDist == -1){
+            return rightDist+1;
+        }
+        else{
+            return leftDist+1;
+        }
+    }
     //main
      /*
                1
@@ -69,8 +83,8 @@ public class MinDistBtw2Nodes {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
         int n1 = 4;
-        int n2 = 5;
-        System.out.println(findLca(root,n1, n2).data);
+        int n2 = 7;
+        System.out.println(minDist(root, n1, n2));
     }
 
     
