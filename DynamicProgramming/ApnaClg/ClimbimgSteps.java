b4372a74777ee5d9944c87cc9a1aa2c28bd582da
mohanreddy3 <mohansaireddykammakatla@gmail.com>
2024-12-24 12:59:23 +0530

    24-12-24

--- DynamicProgramming/ApnaClg/ClimbimgSteps.java
+++ DynamicProgramming/ApnaClg/ClimbimgSteps.java
@@ -7,13 +7,14 @@ import java.util.Arrays;
 public class ClimbimgSteps {
     //main
     public static void main(String[] args) {
-        int n=6; // 5th step
+        int n=5; // 5th step
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
         System.out.println(countSteps(n, dp));
+        System.out.println(countWays(n));
 
     }
-    //method 1 -> (memoization) to calculate no of ways to reach Nth stair
+    //method 1 -> (memoization) to calculate no of ways to reach Nth stair (Top Down Approach)
     private static int countSteps(int n , int[] dp){
         if( n == 0){
             return 1;
@@ -28,6 +29,21 @@ public class ClimbimgSteps {
 
          return dp[n];
 
+    }
+    //method 2 (Tabulation) -> Bottom Up Approach
+    private static int countWays(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int i=1;i<=n; i++){
+            if(i == 1){
+                dp[i] = dp[i-1] + 0;
+            }
+            else{
+                dp[i] = dp[i-1]+dp[i-2];
+            }
+            
+        }
+        return dp[n];
     }
     
 }
