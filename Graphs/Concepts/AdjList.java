98b2727ac6107f16e0881277ccfa276ffb3bebd4
mohanreddy3 <mohansaireddykammakatla@gmail.com>
2025-01-06 12:45:47 +0530

    06-01-25

---
+++ Graphs/Concepts/AdjList.java
@@ -0,0 +1,45 @@
+package Graphs.Concepts;
+
+import java.util.ArrayList;
+
+public class AdjList {
+    static class Edge{
+        int src;
+        int dest;
+        int wt;
+        //constructor
+        public Edge(int s, int d, int w){
+            this.src = s;
+            this.dest = d;
+            this.wt = w;
+        }
+    }
+
+    public static void main(String[] args) {
+        //no of vertices
+        int  V = 5;
+        //int[] arr = new int[size]
+        ArrayList<Edge>[] graph = new ArrayList[V]; // at every index null is stored
+        //create an Empty ArrayList at every index in the Array.
+        for(int i=0; i<V; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        //0 vertex
+        graph[0].add(new Edge(0, 1, 5));
+
+        //1 vertex
+        graph[1].add(new Edge(1, 0, 5));
+        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 3, 3));
+
+        //2 vertex
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 2));
+    
+
+
+    }
+    
+}
