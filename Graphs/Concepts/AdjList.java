44bfd9211717aa5cc71e58eec21380b5cc957ff9
mohanreddy3 <mohansaireddykammakatla@gmail.com>
2025-01-06 19:57:58 +0530

    06-01-25

--- Graphs/Concepts/AdjList.java
+++ Graphs/Concepts/AdjList.java
@@ -37,6 +37,19 @@ public class AdjList {
         graph[2].add(new Edge(2, 1, 1));
         graph[2].add(new Edge(2, 3, 1));
         graph[2].add(new Edge(2, 4, 2));
+
+        //3 vertex
+        graph[3].add(new Edge(3,1,3));
+        graph[3].add(new Edge(3,2,1));
+
+        //4 vertex
+        graph[4].add(new Edge(4,2,2));
+
+        //Node 2's Neighbors
+        for(int i=0; i<graph[2].size(); i++){
+            Edge e = graph[2].get(i); // src , dest , wt
+            System.out.println(e.dest);
+        }
     
 
 
