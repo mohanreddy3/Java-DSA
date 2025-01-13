6b53e344c55e845a27b46bc11dfbb744eeef4c2b
mohanreddy3 <mohansaireddykammakatla@gmail.com>
2025-01-13 23:47:25 +0530

    13-01-25

--- Lambda_Expressions/L1.java
+++ Lambda_Expressions/L1.java
@@ -10,12 +10,15 @@ interface A{
 }
 public class L1 {
     public static void main(String[] args) {
+        
         // A obj = new A() { // Anonymous Inner Class (traditional way)
         //     public void greet(){
         //         System.out.println("Heyy there!");
         //     }   
         // };
+
         // Using Lambda Expression
+
         A obj = () -> System.out.println("Hey there!!"); 
         //lambda exp knows which method to point coz it's a Functional Interface (only 1 method)
         obj.greet();
